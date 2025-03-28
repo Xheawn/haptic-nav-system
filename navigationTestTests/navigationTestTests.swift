61f74d4903dfc1799a6507e8eab0999b397c9cf2
Xheawn <yinxiong2005@gmail.com>
2025-03-27 19:06:35 -0700

    Initial Commit

---
+++ navigationTestTests/navigationTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  navigationTestTests.swift
+//  navigationTestTests
+//
+//  Created by 殷雄 on 3/27/25.
+//
+
+import Testing
+@testable import navigationTest
+
+struct navigationTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
